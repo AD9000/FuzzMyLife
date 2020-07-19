237424439105fb317ea60e99d8cccf3fcd37c393
AD9000 <damle.atharv5@gmail.com>
2020-07-19 00:43:35 -0400

    cleanup, updated readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Binary fuzzer for 6447
 
 Credits:
--Atharv Damle
--Lasindu Shanil de Silva
--Song Fang
--Justin Mack
+- Atharv Damle
+- Lasindu Shanil de Silva
+- Song Fang
+- Justin Mack
--- src/gen_fuzz.py
+++ src/gen_fuzz.py
@@ -5,51 +5,16 @@ import copy
 
 from log import *
 
-# hardcode these in a wordlists file
-# research how to actually make a good fuzzer.
+# put these in a wordlists file
 intcases = [-1, 0, 1, 10*(2**20), -10*(2**20)]
 stringcases = ["A"*10, "A"*100, "A"*1000, "\'", "\"", "\\", "#", "%", "--", " ", "\n", "`", ",", ".", "/", ""]
 formatcases = ["%n"*10, "%n"*100, "%1000000$x"]
-
-# We should have a function that makes strings of ever increasing length for overflow test
-
-# global binary
-
-# return inputWords dict that causes crash, or None if no crash
-# based on speed so far I estimate it will take more than one hour to run completely on csv
-# options^fields ie completely prohibitive
-# def permutations(inputWords: dict, i: int) -> dict:
-#     if i == len(inputWords['values']):
-#         return send(inputWords)
-#     else:
-#         testcases = [inputWords['values'][i]] # original value
-#         if isinstance(inputWords['values'][i], int):
-#             testcases += intcases
-#         elif isinstance(inputWords['values'][i], str):
-#             testcases += stringcases + formatcases
-#         else:
-#             raise()
-#         # float
-#         # other types?
-
-#         for case in testcases:
-#             inputWords['values'][i] = case
-#             res = permutations(inputWords, i+1)
-#             if res is not None:
-#                 return res
-#         return None
 
 # did you mean fastFuzz
 # you demon
 def fast_fuzz(inputWords: dict) -> str:
     for i in range(len(inputWords['values'])):
         testcases = [inputWords['values'][i]] + intcases + stringcases + formatcases
-        # if isinstance(inputWords['values'][i], int):
-        #     testcases += intcases
-        # elif isinstance(inputWords['values'][i], str):
-        #     testcases += stringcases + formatcases
-        # else:
-        #     raise()
 
         for case in testcases:
             payload = copy.deepcopy(inputWords)
@@ -77,6 +42,5 @@ def send(words: dict) -> str:
 def generalFuzz(_binary: str, inputWords: dict) -> str:
     global binary 
     binary = _binary
-    # crashInput = permutations(inputWords.copy(), 0)
     crashInput = fast_fuzz(inputWords.copy())
     return crashInput
