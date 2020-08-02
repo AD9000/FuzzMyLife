8a27441e3af7b4d105a92f223a5af6d66a6998d8
ecl1ipse <shanildesilva@gmail.com>
2020-08-02 17:18:51 +1000

    got mapping running

--- src/map.py
+++ src/map.py
@@ -5,28 +5,33 @@ from log import *
 
 def mapFuzz(inputWords: dict):
     cases = ["A", 10]
-    for i in inputWords:
-        cases.append(inputWords[i])
+    newWords = copy.deepcopy(inputWords)
+    for i in range (0, len(inputWords["values"])):
+        cases.append(inputWords["values"][i])
         for j in cases:
-            payload = copy.deepcopy(inputWords)
+            payload = copy.deepcopy(inputWords["values"])
             payload[i] = j
-            mapSend(payload)
-        cases.remove(inputWords[i])
+            newWords["values"] = payload
+            mapSend(newWords)
+        cases.remove(inputWords["values"][i])
 
 def isolateFields(inputWords: dict, output: str) -> list:
     keyIndexes = []
     cases = ["A", 10]
-    for i in range(0, len(inputWords)):
-        cases.append(inputWords[i])
-        for j in cases:
-            payload = copy.deepcopy(inputWords)
-            payload[i] = j
-            out = mapSend(payload)
-            if out != output:
-                keyIndexes.append(i)
-                break
-        cases.remove(inputWords[i])
+    newWords = copy.deepcopy(inputWords)
 
+    for i in range(0, len(inputWords["values"])):
+        cases.append(inputWords["values"][i])
+        for j in cases:
+            payload = copy.deepcopy(inputWords["values"])
+            payload[i] = j
+            newWords["values"] = payload
+            out = outSend(newWords)
+            if out != output:
+                keyIndexes.append({i: inputWords["values"][i]})
+                break
+        cases.remove(inputWords["values"][i])
+    
     return keyIndexes
 
 def mapSend(words: dict) -> str:
@@ -34,9 +39,18 @@ def mapSend(words: dict) -> str:
 
     p = Popen(binary, stdin=PIPE, stdout=PIPE)
     output, error = p.communicate(inputString.encode())
-
+    
     if output not in binmap:
         binmap[output] = isolateFields(words, output)
+
+    return output
+
+def outSend(words: dict) -> str:
+    inputString = parse.getInputFromDict(words)
+
+    p = Popen(binary, stdin=PIPE, stdout=PIPE)
+    output, error = p.communicate(inputString.encode())
+    
     return output
 
 def mapper(_binary: str, inputWords: dict) -> str:
@@ -45,4 +59,3 @@ def mapper(_binary: str, inputWords: dict) -> str:
     binary = _binary
     mapFuzz(inputWords)
     print(binmap)
-    print(inputWords)
